ef5f4d435b4c58ab4a86f240c04882906928e9d1
Челомов Степан <stepan.chelomov@mail.ru>
2024-10-27 18:22:11 +0300

    fix double compare

--- inc/double_compare.hpp
+++ inc/double_compare.hpp
@@ -3,7 +3,7 @@
 
 namespace Compare {
     
-const double epsilon = 1.0e-10;
+const double epsilon = 1.0e-7;
 
 bool is_equal(const double x, const double y);
 bool is_greater_or_equal(const double x, const double y);
